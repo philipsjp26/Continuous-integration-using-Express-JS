b41cff10e28ef8962d104a890974597c0ffb341a
philipsjp26 <philipsjp12@gmail.com>
2021-01-11 21:54:45 +0700

    change ping

--- app/test/ping.test.js
+++ app/test/ping.test.js
@@ -1,12 +1,18 @@
 let chai = require('chai');
 let app = require('../app');
 let chaiHTTP = require('chai-http');
+const { expect } = require('chai');
 
 chai.use(chaiHTTP);
 
 describe('/ping', () => {
     it('get_ping', (done) => {
-        chai.request(app).get('/ping');
-        done();
+        chai.request(app)
+        .get('/pings')
+        .end((err, res) => {
+            if(err) done(err);
+            expect(res.status).to.equal(200);
+            done();
+        })
     });
 });
